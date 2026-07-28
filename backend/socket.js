d72a2bb1c5a2dcc5b19d620a96176148ea0e5143
Vivek Balla <vivekballa123@email.com>
2026-07-28 23:01:58 +0530

    Fix production socket CORS

--- backend/socket.js
+++ backend/socket.js
@@ -5,41 +5,56 @@ import captainModel from "./models/captain.model.js";
 let ioInstance = null;
 
 export function initializeSocket(server) {
-
     if (ioInstance) return ioInstance;
 
     ioInstance = new Server(server, {
         cors: {
-            origin: ["http://localhost:5173", "https://hopon-backend-63qr.onrender.com"],
+            origin: [
+                "http://localhost:5173",
+                "https://hopon-frontend.onrender.com"
+            ],
             methods: ["GET", "POST"],
-            credentials: true,
-        },
+            credentials: true
+        }
     });
 
     console.log("Socket.IO initialized");
 
     ioInstance.on("connection", (socket) => {
-
         console.log("Client Connected:", socket.id);
 
-        socket.on('join', async (data) => {
+        socket.on("join", async (data) => {
             const { userId, userType } = data;
-            console.log(`User ${userId} of type ${userType} joined with socket ID: ${socket.id}`);
-            if (userType === 'user') {
-                await userModel.findByIdAndUpdate(userId, { socketId: socket.id });
+
+            console.log(
+                `User ${userId} of type ${userType} joined with socket ID: ${socket.id}`
+            );
+
+            if (userType === "user") {
+                await userModel.findByIdAndUpdate(userId, {
+                    socketId: socket.id
+                });
+
                 socket.join(`user_${userId}`);
             }
-            if (userType === 'captain') {
-                await captainModel.findByIdAndUpdate(userId, { socketId: socket.id });
+
+            if (userType === "captain") {
+                await captainModel.findByIdAndUpdate(userId, {
+                    socketId: socket.id
+                });
+
                 socket.join(`captain_${userId}`);
             }
         });
-        socket.on('update-location-captain', async (data) => {
+
+        socket.on("update-location-captain", async (data) => {
             const { userId, location } = data;
+
             if (!location || !location.ltd || !location.lng) {
-                console.error('Invalid location data received');
+                console.error("Invalid location data received");
                 return;
             }
+
             await captainModel.findByIdAndUpdate(userId, {
                 location: {
                     type: "Point",
@@ -47,10 +62,10 @@ export function initializeSocket(server) {
                 }
             });
         });
+
         socket.on("disconnect", () => {
             console.log("Client Disconnected:", socket.id);
         });
-
     });
 
     return ioInstance;
@@ -61,12 +76,13 @@ export function getIO() {
 }
 
 export function sendMessageToSocketId(socketId, messageObject = {}) {
-
     console.log("Sending message to socket ID:", socketId, messageObject);
 
     if (!ioInstance) return false;
 
-    ioInstance.to(socketId).emit(messageObject.event, messageObject.data);
+    ioInstance
+        .to(socketId)
+        .emit(messageObject.event, messageObject.data);
 
     return true;
 }
